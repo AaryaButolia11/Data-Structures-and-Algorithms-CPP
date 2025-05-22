3e9ab2c0e574d18890de7be9f8a4fac40575610d
Aarya_Butolia <146535399+AaryaButolia11@users.noreply.github.com>
2025-05-23 01:27:15 +0530

    Add files via upload

---
+++ Arrays/array1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Array declaration
+    int arr[7];
+    cout << arr << endl;
+    // cout << &arr << endl;
+    // both show base addr.
+    cout << "Array created successfully!";
+    return 0;
+}
