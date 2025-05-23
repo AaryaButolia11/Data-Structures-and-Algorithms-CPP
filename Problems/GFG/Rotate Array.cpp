5b322ae87c8de014e3a9c899b2b22c7d304c58e2
Aarya_Butolia <146535399+AaryaButolia11@users.noreply.github.com>
2025-05-23 14:21:03 +0530

    Create Rotate Array.cpp

---
+++ Problems/GFG/Rotate Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+
+  
+    void rotateArr(vector<int>& arr, int d) {
+        // code here
+
+        d = d % arr.size();
+        reverse(arr.begin(), arr.begin() + d);       
+        reverse(arr.begin() + d, arr.end());         
+        reverse(arr.begin(), arr.end());   
+    }
+};
