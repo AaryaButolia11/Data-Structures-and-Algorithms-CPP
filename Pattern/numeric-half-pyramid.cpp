35ee0bfc0f6c5361fc92a31d49ac18a5ff32371f
Aarya_Butolia <146535399+AaryaButolia11@users.noreply.github.com>
2025-05-30 12:31:48 +0530

    Add files via upload

---
+++ Pattern/numeric-half-pyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows;
+    int columns;
+    cout << "Enter no of rows: ";
+    cin >> rows;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << j + 1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
