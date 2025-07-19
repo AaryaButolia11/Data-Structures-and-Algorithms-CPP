d71a3c4064e4bd88f6a815105fb5af7b34b53590
Aarya_Butolia <146535399+AaryaButolia11@users.noreply.github.com>
2025-07-19 23:08:09 +0530

    Create 205. Isomorphic Substring.cpp

---
+++ Problems/leetcode/205. Isomorphic Substring.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int hash[256]={0};
+        int isTCharsMapped[256]={0};
+        for(int i=0;i<s.size();i++)
+        {
+            if(hash[s[i]]==0 && isTCharsMapped[t[i]]==0)
+            {
+                hash[s[i]]=t[i];
+                isTCharsMapped[t[i]]=true;
+            }
+        }
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(char(hash[s[i]])!=t[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
