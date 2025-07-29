a16a8e9a0a081278877a6fd8f7ec7c9ff50dbfba
Aarya_Butolia <146535399+AaryaButolia11@users.noreply.github.com>
2025-07-29 22:32:22 +0530

    Update 28. Find the index of the first occurence of the string.cpp

--- Problems/leetcode/28. Find the index of the first occurence of the string.cpp
+++ Problems/leetcode/28. Find the index of the first occurence of the string.cpp
@@ -11,3 +11,25 @@ public:
         return -1;
     }
 };
+
+
+
+// soln2
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+        for (int i = 0; i <= n - m; i++) {
+            for (int j = 0; j < m; j++) {
+                if (needle[j] != haystack[i + j]) {
+                    break;
+                }
+                if (j == m - 1) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};
