9fd8809344f31bb47996deecad97f09a2ba521a7
Aarya_Butolia <146535399+AaryaButolia11@users.noreply.github.com>
2025-07-30 16:21:10 +0530

    Create 15. 3SUM.cpp

---
+++ Problems/leetcode/15. 3SUM.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        int n = nums.size();
+        std::vector<std::vector<int>> result;
+
+        for (int i = 0; i < n - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            if (nums[i] > 0) {
+                break;
+            }
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+
+                if (currentSum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+                } else if (currentSum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+};
