f9bbd785f1e20ad205a85855a9f82a83789fb7ea
Aarya_Butolia <146535399+AaryaButolia11@users.noreply.github.com>
2025-08-26 11:59:57 +0530

    Create 152. Maximum Product subarray.cpp

---
+++ Problems/leetcode/152. Maximum Product subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n=nums.size();
+        int max_end=nums[0];
+        int min_end=nums[0];
+        int sum_so_far=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            int temp=max(max(nums[i],nums[i]*max_end),nums[i]*min_end);
+            min_end=min(min(nums[i],nums[i]*max_end),nums[i]*min_end);
+            max_end=temp;
+            sum_so_far=max(sum_so_far,max_end);
+        }
+        return sum_so_far;
+    }
+    
+
+};
