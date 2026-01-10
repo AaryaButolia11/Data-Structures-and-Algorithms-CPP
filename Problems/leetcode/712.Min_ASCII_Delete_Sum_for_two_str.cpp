50ad3d8a05fe4bbdf23bc16585c29b151010a8ab
AARYA BUTOLIA <aaryabutolia@gmail.com>
2026-01-10 21:25:42 +0530

    potd 712 10-01-2026

---
+++ Problems/leetcode/712.Min_ASCII_Delete_Sum_for_two_str.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int m, n;
+    int t[1001][1001];
+    int solve(int i, int j, string &s1, string &s2)
+    {
+        if (i >= m && j >= n)
+            return 0;
+        if (t[i][j] != -1)
+            return t[i][j];
+        if (i >= m)
+            return t[i][j] = s2[j] + solve(i, j + 1, s1, s2);
+        if (j >= n)
+            return t[i][j] = s1[i] + solve(i + 1, j, s1, s2);
+        if (s1[i] == s2[j])
+            return t[i][j] = solve(i + 1, j + 1, s1, s2);
+
+        int del_s1_i = s1[i] + solve(i + 1, j, s1, s2);
+        int del_s2_j = s2[j] + solve(i, j + 1, s1, s2);
+        return t[i][j] = min(del_s1_i, del_s2_j);
+    }
+    int minimumDeleteSum(string s1, string s2)
+    {
+        m = s1.length();
+        n = s2.length();
+        memset(t, -1, sizeof(t));
+        return solve(0, 0, s1, s2);
+    }
+};
