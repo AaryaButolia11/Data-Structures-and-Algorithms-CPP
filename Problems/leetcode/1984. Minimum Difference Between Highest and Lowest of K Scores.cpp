3d477531803c2e247e5ba7777a8781a0b3b3935a
AARYA BUTOLIA <aaryabutolia@gmail.com>
2026-01-25 16:11:37 +0530

    potd 25-01 TC

--- Problems/leetcode/1984. Minimum Difference Between Highest and Lowest of K Scores.cpp
+++ Problems/leetcode/1984. Minimum Difference Between Highest and Lowest of K Scores.cpp
@@ -5,9 +5,9 @@ public:
     {
         if (k == 1)
             return 0;
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); // O(nlogn)
         int diff = INT_MIN;
-        for (int i = 0; i + k - 1 < nums.size(); i++)
+        for (int i = 0; i + k - 1 < nums.size(); i++) // O(n)
         {
             diff = min(diff, nums[i + k - 1] - nums[i]);
         }
