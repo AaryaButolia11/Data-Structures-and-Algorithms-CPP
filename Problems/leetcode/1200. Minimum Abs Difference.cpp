61d07fa9771bc91160b72b63f7f1868eefe85781
AARYA BUTOLIA <aaryabutolia@gmail.com>
2026-01-26 15:08:22 +0530

    potd 26

--- Problems/leetcode/1200. Minimum Abs Difference.cpp
+++ Problems/leetcode/1200. Minimum Abs Difference.cpp
@@ -5,11 +5,11 @@ public:
     {
         int minDiff = INT_MAX;
         int n = arr.size();
-        sort(arr.begin(), arr.end());
-        for (int i = 1; i < n; i++)
+        sort(arr.begin(), arr.end()); // O(nlogn)
+        for (int i = 1; i < n; i++)   // O(n)
             minDiff = min(minDiff, arr[i] - arr[i - 1]);
         vector<vector<int>> res;
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i < n; i++) // O(n)
         {
             if (arr[i] - arr[i - 1] == minDiff)
             {
