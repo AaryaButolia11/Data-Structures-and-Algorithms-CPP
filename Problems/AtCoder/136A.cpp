2b47c0fd5dd5e5020b37216840957aaaaf0432a9
AARYA BUTOLIA <aaryabutolia@gmail.com>
2026-02-24 22:56:37 +0530

    CF A

---
+++ Problems/AtCoder/136A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int p[101], ans[101];
+    //Read input
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> p[i];
+    }
+    //Invert Permutation
+    for (int i = 1; i <= n; i++)
+    {
+        ans[p[i]] = i;
+    }
+    //Print Result
+    for (int i = 1; i <= n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    return 0;
+}
