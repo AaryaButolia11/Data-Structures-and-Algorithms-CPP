f5b48013e20c1bdc8b5bff4ba623b10a20afd71b
AARYA BUTOLIA <aaryabutolia@gmail.com>
2026-03-05 15:00:40 +0530

    POTD 05.03

---
+++ Problems/leetcode/1758.Minimum Changes To Make Alternating Binary String.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    int minOperations(string s)
+    {
+        int count1 = 0, count2 = 0;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+
+            // pattern 0101...
+            if (i % 2 == 0)
+            {
+                if (s[i] != '0')
+                    count1++;
+            }
+            else
+            {
+                if (s[i] != '1')
+                    count1++;
+            }
+
+            // pattern 1010...
+            if (i % 2 == 0)
+            {
+                if (s[i] != '1')
+                    count2++;
+            }
+            else
+            {
+                if (s[i] != '0')
+                    count2++;
+            }
+        }
+
+        return min(count1, count2);
+    }
+};
