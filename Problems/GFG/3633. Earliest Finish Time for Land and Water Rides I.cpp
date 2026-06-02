1168c2c8fa0ebe4feae63a33d805ac1dd3fef5fa
AARYA BUTOLIA <aaryabutolia@gmail.com>
2026-06-02 13:55:50 +0530

    lc potd

--- Problems/GFG/3633. Earliest Finish Time for Land and Water Rides I.cpp
+++ Problems/GFG/3633. Earliest Finish Time for Land and Water Rides I.cpp
@@ -3,15 +3,18 @@ class Solution
     int solve(vector<int> &start1, vector<int> &duration1, vector<int> &start2,
               vector<int> &duration2)
     {
+        // finish time of first ride is the minimum of all finish times of rides in first category
         int finish1 = INT_MAX;
         for (int i = 0; i < start1.size(); i++)
         {
+            // finish time of a ride is the start time of that ride + duration of that ride
             finish1 = min(finish1, start1[i] + duration1[i]);
         }
 
         int finish2 = INT_MAX;
         for (int i = 0; i < start2.size(); i++)
         {
+            // finish time of a ride in the second category is the maximum of its start time and the finish time of the first ride + its duration
             finish2 = min(finish2, max(start2[i], finish1) + duration2[i]);
         }
         return finish2;
