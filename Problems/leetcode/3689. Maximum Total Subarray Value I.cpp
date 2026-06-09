c612f22124eb3e68d27df228c5d081400c68398b
AARYA BUTOLIA <aaryabutolia@gmail.com>
2026-06-09 23:15:16 +0530

    lc potd

---
+++ Problems/leetcode/3689. Maximum Total Subarray Value I.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    long long maxTotalValue(vector<int> &nums, int k)
+    {
+        //total ele in array
+        int n = nums.size();
+        // define max and min ele
+        int maxEle = INT_MIN;
+        int minEle = INT_MAX;
+
+        // find max and min ele from array
+        for (int &num : nums)
+        {
+            maxEle = max(num, maxEle);
+            minEle = min(num, minEle);
+        }
+
+        // as the max sum will be the difference of maximum ele and minimum ele in array
+        // and we can take same subarr again so we multiply the difference by k here to get sum
+        
+        return k * 1L * (maxEle - minEle);
+    }
+};
